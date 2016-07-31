c29c6a606065ecf0b8e219e45e55e35e74b2386f
rangu <rangu@RANGESH>
2016-07-31 14:20:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poistionofparanthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "(((())))";
+            int position = 2;
+            var p = new Program();
+           var output= p.ReturnClosedParanthesisPosition(input, position);
+            Console.WriteLine(output);
+        }
+        int ReturnClosedParanthesisPosition(string input,int openparenthesisposition)
+        {
+            var parenthesisarray = input.ToCharArray();
+            int counter = 1;
+            for (int i =openparenthesisposition; i < parenthesisarray.Length; i++)
+            {
+                if (parenthesisarray[i]=='(')
+                {
+                    counter++;
+                }
+                if (parenthesisarray[i] == ')')
+                {
+                    counter--;
+                }
+                if (counter == 0)
+                {
+                    return i;                    
+                }
+            }
+            return -1;
+        }
+    }
+}
